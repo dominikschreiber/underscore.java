d327227e5df682c3d97e86e1aef4f4850a801399
Dominik Schreiber <domischreib@gmail.com>
2015-02-03 17:11:47 +0100

    added java.util.function.Supplier<T> to replace Callable<T>

---
+++ src/main/java/java/util/function/Supplier.java
@@ -0,0 +1,16 @@
+package java.util.function;
+
+/**
+ * <p>Represents a supplier of results.</p>
+ * <p>There is no requirement that a new or distinct result be returned each time the supplier is invoked.</p>
+ * <p>This is a functional interface whose functional method is {@link #get()}</p>
+ * @param <T> the type of results supplied by this supplier
+ * @see <a href="http://docs.oracle.com/javase/8/docs/api/java/util/function/Supplier.html">Java 8 Supplier</a>
+ */
+public interface Supplier<T> {
+    /**
+     * <p>Gets a result</p>
+     * @return a result
+     */
+    public T get();
+}
